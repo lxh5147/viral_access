032724a32fc6d9d412311a485b6b14e17892420d
lxh5147 <lxh5147@126.com>
2019-07-22 11:29:04 +0800

    Added a main function with model saving function

--- micro_kol_attract_degree_estimator.py
+++ micro_kol_attract_degree_estimator.py
@@ -1,16 +1,15 @@
 from sklearn.linear_model import LogisticRegression
 from   sklearn.model_selection import train_test_split
 from sklearn.metrics.classification import classification_report
-import numpy as np
-
+from sklearn.externals import joblib
 # prepare data
 
 def pre_process_micro_kol_profile(micro_kol):
     # return features for this micro_kol
-    return None
+    pass
 
 def load_data():
-    return None
+    pass
 
 # train and validate the logistic regression model
 def train_model(X,y):
@@ -28,3 +27,12 @@ def test_model(model:LogisticRegression, X, y):
 def predict(model:LogisticRegression,X):
     # prediction the probability of label 0 and 1
     return model.predict_log_proba(X)
+
+def main():
+    X,y = load_data()
+    X_train,X_test, y_train,y_test = train_test_split(X,y,test_size=0.2)
+    lr = train_model(X_train,y_train)
+    report = test_model(lr, X_test,y_test)
+    print(report)
+    # save the model
+    joblib.dump(lr,'attractness_lr.pkl')

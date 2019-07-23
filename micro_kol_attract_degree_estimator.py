2294fcece8a1e477e34c151bd088cb071377111f
lxh5147 <lxh5147@126.com>
2019-07-23 13:16:50 +0800

    Added load data from CSV

--- micro_kol_attract_degree_estimator.py
+++ micro_kol_attract_degree_estimator.py
@@ -1,38 +1,54 @@
-from sklearn.linear_model import LogisticRegression
-from   sklearn.model_selection import train_test_split
-from sklearn.metrics.classification import classification_report
+import csv
+
+import numpy as np
 from sklearn.externals import joblib
-# prepare data
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics.classification import classification_report
+from sklearn.model_selection import train_test_split
+
 
 def pre_process_micro_kol_profile(micro_kol):
     # return features for this micro_kol
-    pass
+    # field 0, field 1, field 2, 3 and label 0 and 1
+    return [int(micro_kol[-3]), int(micro_kol[-2])], [int(micro_kol[-1])]
 
-def load_data():
-    pass
+
+def load_data(csv_file_path):
+    # TODO: validate the input file
+    X = []
+    y = []
+    with open(csv_file_path, 'rb') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            item_x, item_y = pre_process_micro_kol_profile(row)
+            X.append(item_x)
+            y.append(item_y)
+    return np.array(X), np.array(y)
+
 
 # train and validate the logistic regression model
-def train_model(X,y):
+def train_model(X, y):
     lr = LogisticRegression()
-    lr.fit(X,y)
+    lr.fit(X, y)
     return lr
 
-# save the model
 
-def test_model(model:LogisticRegression, X, y):
+def test_model(model: LogisticRegression, X, y):
     y_pred = model.predict(X)
     return classification_report(y, y_pred)
 
+
 # run the prediction
-def predict(model:LogisticRegression,X):
+def predict(model: LogisticRegression, X):
     # prediction the probability of label 0 and 1
     return model.predict_log_proba(X)
 
+
 def main():
-    X,y = load_data()
-    X_train,X_test, y_train,y_test = train_test_split(X,y,test_size=0.2)
-    lr = train_model(X_train,y_train)
-    report = test_model(lr, X_test,y_test)
+    X, y = load_data()
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+    lr = train_model(X_train, y_train)
+    report = test_model(lr, X_test, y_test)
     print(report)
     # save the model
-    joblib.dump(lr,'attractness_lr.pkl')
+    joblib.dump(lr, 'attractness_lr.pkl')
